aefc9f53daef93bd786d70fd110ad54fa2c3875f
Arthur Reichert <arthur.reichert@crmpiperun.com>
2022-11-26 17:54:36 -0300

    value size

--- lib/presentation/screens/widgets/account_balance.dart
+++ lib/presentation/screens/widgets/account_balance.dart
@@ -12,22 +12,26 @@ class AccountBalance extends StatefulWidget {
 class _AccountBalanceState extends State<AccountBalance> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Row(
-          children: [
-            Text(
-              'Conta',
-              style: TextStyle(fontWeight: FontWeight.bold),
-            ),
-            Icon(Icons.arrow_forward_ios_outlined),
-          ],
-        ),
-        Text(
-          'R\$ 10.000,00',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: Column(
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                'Conta',
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+              ),
+              Icon(Icons.arrow_forward_ios_outlined),
+            ],
+          ),
+          Text(
+            'R\$ 10.000,00',
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 16),
+          ),
+        ],
+      ),
     );
   }
 }
