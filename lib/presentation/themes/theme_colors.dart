7a733de2e1e4ee5fae85a577b893388cbecac938
Arthur Reichert <arthur.reichert@crmpiperun.com>
2022-11-26 17:16:14 -0300

    Colors of App

--- lib/presentation/themes/theme_colors.dart
+++ lib/presentation/themes/theme_colors.dart
@@ -1,10 +1,18 @@
+// ignore_for_file: use_full_hex_values_for_flutter_colors
+
 import 'package:flutter/material.dart';
 
 class AppColor {
 
   //Principals
-  static const Color primaryColor = Color(0XFF993399);
-  static const backgroundColor = Color(0XFFFFFF);
+  static const primaryColor = Color(0XFF830AD1);
+  static const backgroundPageColor = Color(0xFFFFFF);
+  static const backgroundGreyColor = Color(0XFFF0F1F5);
+  static const boldTextPurpleColor = Color(0XFF8B39C4);
+  static const greenColor = Color(0XFF548E70);
 
-  //Other
+  //Others
+  static const blackColor = Color(0XFF00000);
+  static const whiteColor = Color(0xFFFFFF);
+
 }
--- lib/presentation/themes/theme_data.dart
+++ lib/presentation/themes/theme_data.dart
@@ -3,6 +3,6 @@ import 'package:nubank_app/presentation/themes/theme_colors.dart';
 
 ThemeData appTheme(BuildContext context) {
   return ThemeData(
-    backgroundColor: AppColor.backgroundColor
+    backgroundColor: AppColor.backgroundPageColor
   );
 }
